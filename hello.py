a7d18bcf6f12627754326a3530e8f933ffee99f7
Nat Eagan <94456319+neagan01@users.noreply.github.com>
2023-10-10 09:49:19 -0400

    update

---
+++ hello.py
@@ -0,0 +1,1 @@
+print("Hello! Thanks for running hello.py!")
---
+++ hello_numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+print("Hello! Thanks for running hello_numpy.py!")
+
+nparray = np.array(['1,2,3,4'])
+
+print('Here is a numpy array:', nparray)
